e5893e5c64d4a91bc9cce0a207725b571c370d85
Sans Bhatia <sanz1475@yonsei.ac.kr>
2021-09-28 12:18:48 +0900

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseApp = initializeApp({ 
-  apiKey: "AIzaSyDWeYTflMJJqrWHbHPuyNgetFN8o6asayc",
-  authDomain: "cloudmoon-3c5d2.firebaseapp.com",
-  projectId: "cloudmoon-3c5d2",
-  storageBucket: "cloudmoon-3c5d2.appspot.com",
-  messagingSenderId: "540432198582",
-  appId: "1:540432198582:web:f79ac1686fd20ba113ef17",
-  measurementId: "G-1TPQSBZYR2"
+  apiKey: "ENTER YOUR KEY",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
  });
  
 let db = getFirestore(firebaseApp)
